40094f4a6e5c2734e7fddddc08731a4184462cba
Ubuntu <ubuntu@ip-172-31-35-194.us-west-2.compute.internal>
2013-07-09 14:04:24 +0000

    require fs

--- web.js
+++ web.js
@@ -4,6 +4,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
 //  response.send('Hello World 2!');
+  var fs = require('fs')
   response.send(fs.readFileSync("index.html").toString("utf-8"));
 });
 
